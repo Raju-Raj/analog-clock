4359301054b4947d3c253edbaf9fb1748a8a1020
Bojja Raju <brajuraj1997@gmail.com>
2021-11-18 16:41:26 +0530

    analog clock

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ function setTime(){
     const date=time.getDate();
     const hours=time.getHours();
 
-    const hoursForClock=hours >=13? hours%12:hours;
+    const hoursForClock=hours >=13 ? hours % 12 : hours;
     const minutes=time.getMinutes();
     const seconds=time.getSeconds();
     const ampm=hours >= 12 ? 'PM' : 'AM'
@@ -36,20 +36,20 @@ function setTime(){
     hourEl.style.transform=`translate(-50%,-100%) 
     rotate(${scale(hoursForClock,0,11,0,359)}deg)`
 
-    minuteEl.style.transform=`translate(-50%',-100%)
+    minuteEl.style.transform=`translate(-50%,-100%)
     rotate(${scale(minutes,0,59,0,359)}deg)`
 
     secondEl.style.transform=`translate(-50%,-100%)
     rotate(${scale(seconds,0,59,0,359)}deg)`
 
-    timeEl.innerHTML=`${hoursForClock}:${minutes<10 ? `0${minutes}`:minutes} ${ampm}`
+    timeEl.innerHTML=`${hoursForClock}:${minutes < 10 ? `0${minutes}`:minutes} ${ampm}`
     dateEl.innerHTML=`${days[day]},${months[month]}<span class="circle">${date}</span>`
 
 
 }
 
 const scale=(num, in_min, in_max, out_min, out_max) =>{
-    return  (num-in_min)*(out_max-out_min)/(in_max-in_min)+out_min;
+    return (num-in_min)*(out_max-out_min)/(in_max-in_min)+out_min;
 }
 
 setTime()
